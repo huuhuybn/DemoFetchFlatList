128374f0452e72f4dd7c688edc70e6fd74935894
HuyNguyen <huuhuybn@gmail.com>
2020-09-29 22:11:30 +0700

    aaaa

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image, FlatList} from 'react-native';
+import {useEffect, useState} from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const url = 'https://jsonplaceholder.typicode.com/posts/';
+
+    const [data, setData] = useState();
+
+    const [arr, setArr] = useState([]);
+
+    const image_url = 'https://gv.poly.edu.vn/images/logo.png';
+
+    // chương trình chạy vào 1 lần trước khi render (trước khi chạy hàm return)
+    useEffect(() => {
+
+        fetch(url)
+            // chuyen du lieu ve dang json
+            .then((response) => response.json())
+            // duoc goi khi ket thuc request du lieu
+            .then((responseJson) => {
+                setArr(responseJson);
+            })
+            // goi khi xay ra loi request
+            .catch((error) => {
+                console.error(error);
+            });
+    })
+    return (
+        <View style={styles.container}>
+            <FlatList style={{flex: 1}}
+                      data={arr}
+                      renderItem={({item}) => (
+                          <View style={{borderWidth: 1, margin: 8}}>
+                              <Text>{item.title}</Text>
+                              <Text>{item.body}</Text>
+                          </View>
+                      )}
+            />
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
